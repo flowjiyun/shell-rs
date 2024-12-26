d34b2967d06c68c0558223e223a2b41803110697
flowjiyun <gyp2795@gmail.com>
2024-12-26 22:47:46 +0900

    add $

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::io::{self, Write};
 
 fn main() {
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
