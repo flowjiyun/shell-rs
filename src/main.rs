9e0efc60df15f63f8ed7d610444897363fd72921
flowjiyun <gyp2795@gmail.com>
2024-12-26 23:10:01 +0900

    builtin exit implement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+use builtin::exit::exit;
+
+mod builtin;
 fn main() {
     loop {
         print!("$ ");
@@ -9,6 +12,21 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim()); 
+
+        let command_line = input.split(" ").collect::<Vec<&str>>();
+        let builtin = command_line[0].trim();
+        match builtin {
+            "exit" => {
+                let code = if command_line.len() > 1 {
+                    command_line[1].trim().parse::<i32>().unwrap()
+                } else {
+                    0
+                };
+                exit(code);
+            }
+            _ => {
+                println!("{}: command not found", builtin);
+            }
+        }
     }
 }
