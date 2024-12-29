11b20b8283d8a7fad65072cd78715d2aa83cfb41
flowjiyun <gyp2795@gmail.com>
2024-12-29 23:36:47 +0900

    feat: add type fucntion - check path

--- src/builtin/bt_type.rs
+++ src/builtin/bt_type.rs
@@ -1,14 +1,25 @@
+use std::fs;
+
 use crate::BUILTIN_SET;
 
 pub fn bt_type(args: Vec<String>) {
-    if let Some(builtin) = args.get(0) {
-        match BUILTIN_SET.contains(builtin) {
-            true => {
-                println!("{} is a shell builtin", builtin);
-            },
-            false => {
-                println!("{}: not found", builtin);
+    if let Some(prog) = args.get(0) {
+        // first check if the prog is builtin
+        if BUILTIN_SET.contains(prog) {
+            println!("{} is a shell builtin", prog);
+            return;
+        }
+        // then check if the prog is in the PATH
+        let env_path = std::env::var("PATH").unwrap_or(String::default());
+        let paths: Vec<&str> = env_path.split(":").collect();
+        for path in paths {
+            // check if the file exists
+            let file_path = format!("{}/{}", path, prog);
+            if fs::metadata(&file_path).is_ok() {
+                println!("{} is {}", prog, file_path);
+                return;
             }
         }
+        println!("{}: not found", prog);
     }
 }
