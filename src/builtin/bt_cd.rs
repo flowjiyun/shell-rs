52663fd26d287de63094de4ea12007f12084cadd
flowjiyun <gyp2795@gmail.com>
2025-01-01 15:37:10 +0900

    feat: add cd builtin

--- src/builtin/bt_cd.rs
+++ src/builtin/bt_cd.rs
@@ -1,7 +1,13 @@
 pub fn bt_cd(args: Vec<String>) {
     if let Some(dir) = args.get(0) {
+        if dir == "~" {
+            let home_dir = std::env::var("HOME").unwrap_or_default();
+            if let Err(_e) = std::env::set_current_dir(&home_dir) {
+                println!("cd: {}: No such file or directory", home_dir);
+            }
+            return;
+        }
         if let Err(_e) = std::env::set_current_dir(dir) {
-            // cd: /does_not_exist: No such file or directory
             println!("cd: {}: No such file or directory", dir);
         }
     }
